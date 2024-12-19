df2c08e0f36bbb5d5350fa3a99bfdf9789532558
Abdelrahman Emad <bgbody5@gmail.com>
2024-12-19 16:21:11 +0200

    NO MSG

--- server/app.js
+++ server/app.js
@@ -18,12 +18,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(morgan('dev'))
-app.use(cors({ origin: 'https://3d-portfolio-client.vercel.app', credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
 
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/projects', projectRoutes)
 app.use('/api/v1/services', serviceRoutes)
 app.use('/api/v1/contact', contactRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to 3D Portfolio API')
+})
 
 app.all('*', (req, res) => {
   res.status(404).send('OOPS!! 404 page not found')
